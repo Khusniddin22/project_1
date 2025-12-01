b9f45bb8b1fdc488a77f201d95f135c6c7b2ba5d
Khisniddin <you@example.com>
2025-12-01 19:51:18 +0300

    created project and add function greeting

--- src/project_1/utils.py
+++ src/project_1/utils.py
@@ -1,8 +1,10 @@
 import datetime
 import json
 import os
+import pandas as pd
+import numpy as np
 
-path_file = os.path.join()
+path_file = os.path.join('..', 'data', 'operations.xlsx')
 
 def greeting()->str:
     '''Функция приветствия относительно текущего часа'''
@@ -17,6 +19,12 @@ def greeting()->str:
     else:
         return 'Добрый вечер'
 
-def financial_operations(path_file):
-    pass
+def financial_operations_json(path_file: str):
+    '''Функция считывает данные Excel-файла и переводит в json формат'''
+    df_excel =pd.read_excel(path_file)
+    print(df_excel)
+    df_excel = df_excel.replace({np.nan: None})
+    #trans_json = json.dumps(df_excel, ensure_ascii=False)
+    #print(trans_json.encode('utf-8').decode('utf-8'))
 
+financial_operations_json(path_file)
