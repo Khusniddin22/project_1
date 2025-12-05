0b812a1e0f45bd6dcab5026a7b19222b9d037b92
Khisniddin <you@example.com>
2025-12-05 20:30:37 +0300

    add page main

--- src/project_1/utils.py
+++ src/project_1/utils.py
@@ -1,12 +1,20 @@
 import datetime
+from datetime import date, timedelta
 import json
 import os
+
+import requests
 import pandas as pd
-import numpy as np
+from pandas import DataFrame
 
-path_file = os.path.join('..', 'data', 'operations.xlsx')
+from dotenv import load_dotenv
+load_dotenv(".env")
 
-def greeting()->str:
+path_file = os.path.join('..', '..', 'data', 'operations.xlsx')
+path_json = os.path.join('..', '..', 'data', 'user_settings.json')
+
+
+def time_for_greeting()->str:
     '''Функция приветствия относительно текущего часа'''
     now = datetime.datetime.now()
     current_now = now.hour
@@ -19,12 +27,161 @@ def greeting()->str:
     else:
         return 'Добрый вечер'
 
-def financial_operations_json(path_file: str):
-    '''Функция считывает данные Excel-файла и переводит в json формат'''
-    df_excel =pd.read_excel(path_file)
-    print(df_excel)
-    df_excel = df_excel.replace({np.nan: None})
-    #trans_json = json.dumps(df_excel, ensure_ascii=False)
-    #print(trans_json.encode('utf-8').decode('utf-8'))
 
-financial_operations_json(path_file)
+def get_data_time(date_time: str, date_format: str="%Y-%m-%d %H:%M:%S")->list[str]:
+    '''
+    Функция возвращает период от начала месяца до выбранной даты в виде СПИСКА
+    '''
+    dt = datetime.datetime.strptime(date_time, date_format)
+    beginning_month = dt.replace(day=1)
+
+    return [
+        beginning_month.strftime("%d.%m.%Y %H:%M:%S"),
+        dt.strftime("%d.%m.%Y %H:%M:%S")
+    ]
+
+
+def get_period(path_file: str, period: list)->DataFrame:
+    '''
+    Принимает путь к файлу Excel и списку дат и
+    возвращает таблицу (DataFrame) в заданном периоде
+    '''
+    df_excel = pd.read_excel(path_file, sheet_name='Отчет по операциям')
+
+    #переводим тип строковые данные колонки "Дата операции" из таблицы excel в тип datetime
+    df_excel['Дата операции'] = pd.to_datetime(df_excel['Дата операции'], dayfirst=True)
+
+    #задаем начало и конец периода, взяв список дат из get_data_time
+    beginning_date = datetime.datetime.strptime(period[0], "%d.%m.%Y %H:%M:%S")
+    end_date = datetime.datetime.strptime(period[1], "%d.%m.%Y %H:%M:%S")
+
+    #создаем отфильтрованную таблицу в диапазоне периода
+    df_filetered_excel = df_excel[
+        (df_excel['Дата операции'] >= beginning_date) &
+        (df_excel['Дата операции'] <= end_date)
+    ]
+
+    #сортируем отфильтрованную таблицу по возрастанию дат
+    df_sorted = df_filetered_excel.sort_values(by='Дата операции')
+
+    return df_sorted
+
+
+def get_cost_of_card(df_sorted: DataFrame)->list[dict]:
+    '''
+    Функция принимает отсортированную таблицу по периоде из функции get_period и
+    возвращает список словаря, в котором хранятся
+    последние 4 цифры карты, сумма затрат и общий кешбэк
+    '''
+    card_transactions = []
+    card_sorted = df_sorted[
+        [
+            'Номер карты',
+            'Сумма операции с округлением',
+            'Кэшбэк',
+            'Сумма операции',
+        ]
+    ]
+
+    for index, row in card_sorted.iterrows():
+        if row['Сумма операции'] < 0:
+            last_digits = str(row['Номер карты']).replace('*', '')
+            total_spent = row['Сумма операции с округлением']
+            cashback = total_spent / 100
+            card_info = {
+                'last_digits': f'{last_digits}',
+                'total_spent': f'{total_spent}',
+                'cashback': f'{cashback}'
+            }
+            card_transactions.append(card_info)
+
+    return card_transactions
+
+
+def get_top_transactions(df_sorted: DataFrame, number)->list[dict]:
+    '''
+    Функция принимает таблицу (DataFrame) и количество транзакций и
+    возвращает указанное количество топ транзакций
+    '''
+    top_transactions = []
+    df_sorted_pay = df_sorted.sort_values(by='Сумма операции', ascending=False)
+
+    #оставляем в выборке только number строк таблицы
+    top_pay = df_sorted_pay.head(number)
+
+    transactions = top_pay[
+        [
+            'Дата платежа',
+            'Сумма операции',
+            'Категория',
+            'Описание'
+        ]
+    ]
+    for index, row in transactions.iterrows():
+        top_info = {
+            'date': row['Дата платежа'],
+            'amount': row['Сумма операции'],
+            'category': row['Категория'],
+            'description': row['Описание']
+        }
+        top_transactions.append(top_info)
+
+    return top_transactions
+
+
+def get_currency_rates(path_json: str)->list[dict]:
+    '''
+    Функция принимает путь до json-файла и возвращает курс валют
+    '''
+    currency_rates = []
+    with open(path_json, 'r', encoding='utf-8') as f_cur:
+        data = json.load(f_cur)
+        currencies = data['user_currencies']
+
+        for currency in currencies:
+            API_KEY_CURRENCY = os.getenv("API_KEY_CURRENCY")
+            rub = "RUB"
+            headers = {
+                'apikey': API_KEY_CURRENCY
+            }
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to={rub}&from={currency}&amount={1}"
+            response = requests.get(url, headers=headers)
+
+            status_code = response.status_code
+            if status_code == 200:
+                result = response.json()
+                currency_info = {
+                    "currency": result['query']['from'],
+                    "rate": round(result['result'], 2)
+                }
+                currency_rates.append(currency_info)
+    return currency_rates
+
+
+def get_stock_prices(path_json: str)->list[dict]:
+    '''
+    Функция принимает путь до json-файла и возвращает цена на акции
+    '''
+    stock_prices = []
+    with open(path_json, 'r', encoding='utf-8') as f_st:
+        data = json.load(f_st)
+        stocks = data['user_stocks']
+        day = date.today()
+        yesterday = str(day - timedelta(days=1))
+
+        for stock in stocks:
+            API_KEY_STOCK = os.getenv('API_KEY_STOCK')
+            URL = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stock}&interval={10}min&apikey={API_KEY_STOCK}'
+            response = requests.get(URL)
+
+            status_code = response.status_code
+            if status_code == 200:
+                result = response.json()
+                stock_info = {
+                    "stock": stock,
+                    "price": result['Weekly Time Series'][yesterday]['2. high']
+                }
+                stock_prices.append(stock_info)
+    return stock_prices
+
+
--- src/project_1/views.py
+++ src/project_1/views.py
@@ -1,2 +1,42 @@
-import requests
+import json
 
+from typing import Dict, Any
+
+from project_1.utils import (
+    time_for_greeting,
+    get_data_time,
+    get_period,
+    get_cost_of_card,
+    get_top_transactions,
+    get_currency_rates,
+    get_stock_prices,
+    path_json
+)
+
+
+def main_info(date_time: str)->Dict[str, Any]:
+    '''
+    Принимает на вход строку с датой и временем в формате YYYY-MM-DD HH:MM:SS
+    и возвращает JSON-ответ
+    '''
+    greeting = time_for_greeting()
+
+    time_period = get_data_time(date_time)
+    sorted_df = get_period("../../data/operations.xlsx", time_period)
+
+    cards = get_cost_of_card(sorted_df)
+    top_transactions = get_top_transactions(sorted_df, 5)
+    currency_rates = get_currency_rates(path_json)
+    stock_prices = get_stock_prices(path_json)
+
+    data = {
+        'greeting': greeting,
+        # 'cards': cards,
+        # 'top_transactions': top_transactions,
+        # 'currency_rates': currency_rates,
+        'stock_prices': stock_prices
+    }
+    json_data = json.dumps(data, ensure_ascii=False, indent=4)
+
+
+    return json_data
