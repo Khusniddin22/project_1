ba93aecc25040bf80dd1a2d27ff3fd278e4cdc5d
Khisniddin <you@example.com>
2025-12-22 00:06:01 +0300

    code is formatted by standard

--- src/project_1/main.py
+++ src/project_1/main.py
@@ -1,4 +1,5 @@
-from views import main_info, even_info
+from views import even_info, main_info
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    print(main_info("2018-05-20 15:30:00"))
     print(even_info("2018-05-20 15:30:00"))
